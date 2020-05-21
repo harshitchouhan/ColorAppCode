d7bef16902153e3fbfc217db89b614c67965858c
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-05-21 19:59:23 +0530

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColor';
+import { generatePalettes } from './colorHelpers';
 
-function App() {
-  return (
-    <div className="App">    
-      <Palette {...seedColors[4]} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Palette palette={generatePalettes(seedColors[4])} />
+      </div>
+    );
+  }
 }
 
 export default App;
