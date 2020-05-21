4a5e5f837043a9b8d9edbd3ff7624f6bce7f2980
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-05-21 10:44:40 +0530

    create Palette component

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">
+            
+        </div>
+      </div>
+    );
+  }
+}
